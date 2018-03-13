6139b56334278573be3237d56f4db53254443f20
s-h <apache3@foxmail.com>
2018-03-13 17:34:03 +0800

    update

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -7,5 +7,7 @@ from .models import Question, Choice
 
 
 # Register your models here.
-admin.site.register(Question)
-admin.site.register(Choice)
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+
+admin.site.register(Question, QuestionAdmin)
